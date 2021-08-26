560a93cafce2d7749dbfafe2a9b2e97e0cd4c245
Daniel Cronqvist <dancro99@gmail.com>
2021-08-27 00:29:08 +0200

    Added class for tokens and enumeration for token types

--- lexing/tokens.py
+++ lexing/tokens.py
@@ -1,2 +1,29 @@
+from enum import Enum
+
+class TokenType(Enum):
+    # Single-character tokens.
+    PLUS = "+"
+    MINUS = "-"
+    STAR = "*"
+    SLASH = "/"
+    BANG = "!"
+
+    # Literal values.
+    INTEGER = "INT"
+    FLOAT = "FLOAT"
+    STRING = "STRING"
+    BOOLEAN = "BOOL"
+
+    # End of file.
+    EOF = 'EOF'
+
 class Token:
-    pass
+    def __init__(self, token_type: TokenType, value=None):
+        self.token_type = token_type
+        self.value = value
+
+    def __repr__(self) -> str:
+        if self.value:
+            return f'<{self.token_type}, {self.value}>'
+        else:
+            return f'<{self.token_type}>'
